ac6ab66445fd34e1243cf4cc3343b296d230e964
Binau02 <aubin.saunier@gmail.com>
2026-08-07 11:26:27 +0200

    create must.java

--- src/main/java/com/piggypig/createwinegrapes/fluids/ModFluids.java
+++ src/main/java/com/piggypig/createwinegrapes/fluids/ModFluids.java
@@ -1,22 +1,25 @@
 package com.piggypig.createwinegrapes.fluids;
 
 import com.piggypig.createwinegrapes.CreateWineGrapes;
+import com.piggypig.createwinegrapes.fluids.custom.Must;
 import com.tterrag.registrate.util.entry.FluidEntry;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.fluids.BaseFlowingFluid;
 
 public class ModFluids {
 
-    public static final FluidEntry<BaseFlowingFluid.Flowing> MUST =
+    public static final FluidEntry<Must> MUST =
             CreateWineGrapes.REGISTRATE
                     .fluid(
                             "must",
                             fluid("red_wine"),
-                            fluid("red_wine")
+                            fluid("red_wine"),
+                            Must::new
                     )
                     .lang("must")
                     .tag(net.minecraft.tags.FluidTags.create(
-                            ResourceLocation.fromNamespaceAndPath(CreateWineGrapes.MOD_ID, "must")))
+                            ResourceLocation.fromNamespaceAndPath(CreateWineGrapes.MOD_ID, "must"))
+                    )
                     .register();
 
     public static final FluidEntry<BaseFlowingFluid.Flowing> ETHANOL =
